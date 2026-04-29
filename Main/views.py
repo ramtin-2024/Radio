8c53045f8b83469a43787090ce7cb52bc9db806b
ramtin <ramtin.windows.11pro@gmail.com>
2026-04-30 00:09:51 +0330

    A function was added to the view file to measure the popularity. If it is positive, it will not be called. If it is negative, it will be called and it will be added to the end of the logic queue.

--- Main/views.py
+++ Main/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from .models import Genre
-from .serializers import GenreSerializer
+from .models import Genre,Music
+from .serializers import GenreSerializer,MusicSerializer
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
+from .paginations import MusicPagination
+from rest_framework.permissions import  IsAuthenticatedOrReadOnly
+from django.db.models import F,Case,When,IntegerField
 # Create your views here.
 
 # ==================== Genre ViewSet ====================
@@ -13,4 +15,18 @@ class GenreViewSet(viewsets.ModelViewSet):
     filterset_fields = ['name']
     http_method_names = ['get', 'post', 'put', 'patch']
 
-    
+# ==================== Music ViewSet ====================
+class MusicModelViewSet(viewsets.ModelViewSet):
+  serializer_class =MusicSerializer
+  queryset=Music.objects.all().order_by('uploaded_at')
+  permission_classes = [IsAuthenticatedOrReadOnly]
+  pagination_class = MusicPagination
+
+  def get_queryset(self):
+        qs = Music.objects.all().order_by('uploaded_at')
+        if self.request.query_params.get('order_in_genre') == '2':
+            qs = qs.annotate(
+                is_negative=Case(
+                    When(like__lt=F('dislike'), then=1),default=0,output_field=IntegerField(),)
+            ).order_by('is_negative', 'uploaded_at')
+        return qs
